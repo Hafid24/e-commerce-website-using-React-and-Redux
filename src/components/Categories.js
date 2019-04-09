85c91d0e36a271a6a296a291df7a210290d77a02
Hafid24 <ilymassh@gmail.com>
2019-04-09 09:26:47 -0700

    removing if statements

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,47 +3,19 @@ import {Link} from 'react-router-dom'
 import '../style/Categories.css'
 
 export default class Categories extends Component {
- getClass=(category)=>{
-   let classes = [];
-   if(category=='All'){
-      classes.push("list-group-item active");
-   }else{
-    classes.push("list-group-item");
-   }
-   if(category.toLowerCase()=='apple'){
-    classes.push("list-group-item active");
- }else{
-  classes.push("list-group-item");
- }
-if(category.toLowerCase()=='google'){
-  classes.push("list-group-item active");
-}else{
-classes.push("list-group-item");
-}
-if(category.toLowerCase()=='htc'){
-  classes.push("list-group-item active");
-}else{
-classes.push("list-group-item");
-}
-if(category.toLowerCase()=='samsung'){
-  classes.push("list-group-item active");
-}else{
-classes.push("list-group-item");
-}
-return classes;
- }
 
   render() {
-     const classes = this.getClass(this.props.category)
+     const {category} = this.props
+    
     return (
       <div className="categories-container">
       <h3>Categories</h3>
             <div class="list-group">
-                    <Link to={"/"} className={classes[0]}>All</Link>
-                    <Link to={"/brand/apple"} className={classes[1]}>Apple</Link>   
-                    <Link to={"/brand/google"} className={classes[2]}>Google</Link>
-                    <Link to={"/brand/htc"} className={classes[3]}>HTC</Link>
-                    <Link to={"/brand/samsung"} className={classes[4]}>Samsung</Link>
+                    <Link to={"/"} className={(category=== 'All') ?  ("list-group-item active"):("list-group-item")}>All</Link>
+                    <Link to={"/brand/apple"} className={(category=== 'apple') ?  ("list-group-item active"):("list-group-item")}>Apple</Link>   
+                    <Link to={"/brand/google"} className={(category=== 'google') ?  ("list-group-item active"):("list-group-item")}>Google</Link>
+                    <Link to={"/brand/htc"} className={(category=== 'htc') ?  ("list-group-item active"):("list-group-item")}>HTC</Link>
+                    <Link to={"/brand/samsung"} className={(category=== 'samsung') ?  ("list-group-item active"):("list-group-item")}>Samsung</Link>
             </div>
       </div>
          
