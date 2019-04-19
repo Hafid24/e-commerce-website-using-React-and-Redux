b0fc70def15a5f194015074af795609bb8408afa
Hafid24 <34862888+Hafid24@users.noreply.github.com>
2019-04-19 08:14:26 -0700

    adding search field feature

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -4,11 +4,41 @@ import '../style/Categories.css'
 import PropTypes from 'prop-types';
 
 export default class Categories extends Component {
+constructor(props){
+    super(props);
 
-  render() {
+    this.state = {
+        searchValue:''
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onSearchInputChange = this.onSearchInputChange.bind(this);
+};
+
+ 
+handleSubmit = (e)=>{
+      
+  e.preventDefault();
+ if(this.state.searchValue !== ''){
+  this.props.p.history.push('/brand/'+this.state.searchValue)
+ }
+}
+
+onSearchInputChange = (e)=>{
+  const searchValue = e.target.value;
+  console.log(searchValue)
+  this.setState({
+      searchValue
+  });
+
+};
+
+
+render() {
      const {category} = this.props
     
     return (
+    <div>
       <div className="categories-container">
       <h3>Categories</h3>
             <div class="list-group">
@@ -19,12 +49,33 @@ export default class Categories extends Component {
                     <Link to={"/brand/samsung"} className={(category=== 'samsung') ?  ("list-group-item active"):("list-group-item")}>Samsung</Link>
             </div>
       </div>
-         
+      
+      <div className="well blosd">
+                <h3 className="lead"> 
+                    Quick Shop 
+                </h3>
+                <div className="input-group">
+                    <form onSubmit={this.handleSubmit}>
+                        <input 
+                            type="text"
+                            className="form-control"
+                            value={this.state.searchValue}    
+                            onChange={this.onSearchInputChange}
+                        />
+                    </form>
+                    <span className="input-group-btn" onClick = {this.handleSubmit}>
+                        <button className="btn btn-default">
+                            <span className="glyphicon glyphicon-search" />
+                        </button>
+                    </span>
+                </div>
+            </div>
+      </div>
     )
   }
 }
 
 
-Categories.propTypes ={
+Categories.propTypes = {
   category : PropTypes.string
 }
